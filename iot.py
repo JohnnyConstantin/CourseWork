da9802dc9fd5a5461c2f755a224a7d6ea5064c21
Johnny_Work <zkonst12@gmail.com>
2021-11-04 18:28:02 +0300

    Added comments and readme

--- iot.py
+++ iot.py
@@ -46,6 +46,8 @@ def read_data():
                                     Доп. задание 2
 """
 
+
+# Генерация инфы для функций emulate_data() и publish_emulatedData()
 def generate_data():
     for x in range(1, 5):
         data = {"motion": int(random.random()) * x, "temperature": random.random() * x,
@@ -54,6 +56,8 @@ def generate_data():
         jsonstring = json.dumps(data)
         return jsonstring
 
+
+# Юзаем сгенерированную инфу и выплевываем в консоль
 def emulate_data():
     while 1:
         try:
@@ -65,18 +69,22 @@ def emulate_data():
 
 #                                   Практическая 7
 
+
+#                                   Практическая 8
+# Публикуем сгенерированную информацию в топик
 def publish_emulatedData():
     while 1:
         try:
             click.echo(os.system(f"sudo mosquitto_pub -h \"192.168.2.230\" -p \"1883\" -t \""
-                             f"/we/are/legion/\" -m \"{generate_data()}\""))
+                                 f"/we/are/legion/\" -m \"{generate_data()}\""))
             time.sleep(1)
         except (KeyboardInterrupt):
             exit("Передача данных остановлена")
 
 
+#                                   Практическая 8
 if __name__ == '__main__':
-    # catch_data()
-    # read_data()
-    #emulate_data()
-    publish_emulatedData()
+    catch_data()  # //Раскомментить для ПР7 ч1
+    read_data()  # //Раскомментить для ПР7 ч1
+# emulate_data()    #//Раскомментить для ПР7 ч2
+# publish_emulatedData()    #//Раскомментить  для ПР8
--- mosq_reader.py
+++ mosq_reader.py
@@ -1,7 +1,7 @@
 import os
 import click
 
-
+# Читаем топик с брокера и пайпуем сообщения в локальную бд
 def get_data():
     click.echo(os.system("sudo mosquitto_sub -h \"192.168.2.230\" -p \"1883\" -t \""
                          "/we/are/legion/\" -v > log.json"))
